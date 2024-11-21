ea582bca9f8a053ae4e17ef0f59cdf33910205aa
bigtreetech <tech@biqu3d.com>
2024-11-21 12:21:30 +0800

    dbug

--- board/sunxi/board.c
+++ board/sunxi/board.c
@@ -589,8 +589,14 @@ void sunxi_board_init(void)
 	power_failed |= axp_set_dcdc5(CONFIG_AXP_DCDC5_VOLT);
 #endif
 #ifdef CONFIG_AXP_DCDC2_VOLT
-	power_failed |= axp_set_dcdc2(CONFIG_AXP_DCDC2_VOLT);
-	power_failed |= axp_set_dcdc3(CONFIG_AXP_DCDC3_VOLT);
+    power_failed |= axp_set_dcdc1(960);
+    power_failed |= axp_set_dcdc2(1000);
+    power_failed |= axp_set_dcdc3(1500);
+    // power_failed |= axp_set_aldo1(1800);
+    // power_failed |= axp_set_dldo(1,3300);
+
+	// power_failed |= axp_set_dcdc2(CONFIG_AXP_DCDC2_VOLT);
+	// power_failed |= axp_set_dcdc3(CONFIG_AXP_DCDC3_VOLT);
 #endif
 #ifdef CONFIG_AXP_DCDC4_VOLT
 	power_failed |= axp_set_dcdc4(CONFIG_AXP_DCDC4_VOLT);
